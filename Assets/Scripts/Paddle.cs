1e138e82e0b58894208454ac51871bda93001d8d
Can Akdag <canakdag06@gmail.com>
2025-06-25 19:34:26 +0300

    created Paddle.cs

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    private InputReader inputReader;
+    private Vector2 moveInput;
+
+    [SerializeField] private float moveSpeed;
+
+    private void Awake()
+    {
+        inputReader = InputReader.Instance;
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput = inputReader.MoveInput;
+        
+        transform.Translate(moveSpeed * Time.deltaTime * moveInput);
+
+        inputReader.ResetInputs();
+    }
+}
