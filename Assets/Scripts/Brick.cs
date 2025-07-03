f4eea1d20e71885917a8d7667faa5d8fa70a7567
Can Akdag <canakdag06@gmail.com>
2025-07-03 10:51:12 +0300

    updated Brick.cs

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -2,6 +2,50 @@ using UnityEngine;
 
 public class Brick : MonoBehaviour
 {
+    public BrickData data;
+    [SerializeField] private int hitPoints = 1;
+    [SerializeField] private SpriteRenderer renderer;
+
+
+    private void Start()
+    {
+        if (data == null)
+        {
+            Debug.Log("BrickData not assigned", this);
+            return;
+        }
+
+        renderer.color = data.brickColor;
+        UpdateSprite();
+    }
+
+    public void Hit()
+    {
+        if(!data.isBreakable)
+        {
+            return;
+        }
+
+        hitPoints--;
+        if(hitPoints <= 0)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            UpdateSprite();
+        }
+    }
+
+    private void UpdateSprite()
+    {
+        if (data.damageSprites.Length == 0)
+            return;
+
+        int spriteIndex = Mathf.Clamp(data.damageSprites.Length - hitPoints, 0, data.damageSprites.Length - 1);
+        renderer.sprite = data.damageSprites[spriteIndex];
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Destroy(gameObject);
