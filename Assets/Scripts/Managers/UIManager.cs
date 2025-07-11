50611ecb3c73f0a4aeb4976b7b320b30b7860c38
Can Akdag <canakdag06@gmail.com>
2025-07-11 17:44:06 +0300

    updated adding/subtracting life UI

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -1,4 +1,4 @@
-using DG.Tweening;
+﻿using DG.Tweening;
 using TMPro;
 using UnityEngine;
 
@@ -33,11 +33,44 @@ public class UIManager : MonoBehaviour
 
     private void UpdateLivesUI(int currentLives)
     {
+        //for (int i = 0; i < lives.Length; i++)
+        //{
+        //    if (lives[i] != null)
+        //    {
+        //        lives[i].gameObject.SetActive(i < currentLives - 1);
+        //    }
+        //}
+
         for (int i = 0; i < lives.Length; i++)
         {
-            if (lives[i] != null)
+            GameObject lifeObj = lives[i].gameObject;
+            Transform t = lifeObj.transform;
+            UnityEngine.UI.Image image = lifeObj.GetComponent<UnityEngine.UI.Image>();
+
+            bool shouldBeActive = i < currentLives - 1;
+
+            // adding
+            if (!lifeObj.activeSelf && shouldBeActive)
             {
-                lives[i].gameObject.SetActive(i < currentLives - 1);
+                lifeObj.SetActive(true);
+                t.localScale = Vector3.zero;
+                t.DOScale(1f, 0.5f).SetEase(Ease.OutBack);
+            }
+
+            // subtracting
+            else if (lifeObj.activeSelf && !shouldBeActive)
+            {
+                Color originalColor = image.color;
+                image.color = Color.red;
+
+                // being red + scale down → disable
+                t.DOScale(0f, 0.5f).SetEase(Ease.InBack)
+                    .OnComplete(() =>
+                    {
+                        lifeObj.SetActive(false);
+                        t.localScale = Vector3.one;
+                        image.color = originalColor;
+                    });
             }
         }
     }
