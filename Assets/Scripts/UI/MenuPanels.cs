128191f45fd41634ac81b09a1cc6e403932e479d
Can Akdag <canakdag06@gmail.com>
2025-08-08 16:23:04 +0300

    fade out for main menu

--- Assets/Scripts/UI/MenuPanels.cs
+++ Assets/Scripts/UI/MenuPanels.cs
@@ -1,11 +1,15 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuPanels : MonoBehaviour
 {
     public GameObject mainMenuPanel;
     public GameObject levelsPanel;
     public GameObject settingsPanel;
+    [SerializeField] private Image fadeImage;
+    [SerializeField] private float fadeDuration;
 
     public void OpenMainMenu()
     {
@@ -30,6 +34,22 @@ public class MenuPanels : MonoBehaviour
 
     public void PlayGame()
     {
+        StartCoroutine(FadeOutAndLoad());
+    }
+
+    private IEnumerator FadeOutAndLoad()
+    {
+        float t = 0;
+        Color c = fadeImage.color;
+
+        while (t < fadeDuration)
+        {
+            t += Time.deltaTime;
+            c.a = Mathf.Lerp(0, 1, t / fadeDuration);
+            fadeImage.color = c;
+            yield return null;
+        }
+
         SceneManager.LoadScene("Game");
     }
 }
