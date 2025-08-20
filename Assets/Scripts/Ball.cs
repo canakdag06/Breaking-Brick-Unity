82cad2f207ecbba45c38cd2e21d013bf446b056a
Can Akdag <canakdag06@gmail.com>
2025-08-20 16:24:26 +0300

    updated paddle collision

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
@@ -33,31 +33,47 @@ public class Ball : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Paddle"))
         {
-            HandlePaddleCollision(collision);
+            ContactPoint2D contact = collision.GetContact(0);
+            Vector2 normal = contact.normal;
+            //Debug.Log("normal: " + normal);
+
+            if (normal.y > 0.5f)    // the ball hits the top of the Paddle
+            {
+                HandlePaddleCollision(collision);
+            }
+            else
+            {
+                FixBallDirection();
+            }
         }
         else if (collision.gameObject.CompareTag("DeathWall"))
         {
             BallManager.Instance.ReturnBallToPool(this);
-            if(!BallManager.Instance.HasActiveBalls())
+            if (!BallManager.Instance.HasActiveBalls())
             {
                 GameManager.Instance.UpdateLives(false);
             }
         }
         else
         {
-            rb.linearVelocity = rb.linearVelocity.normalized * speed;
-
-            Vector2 v = rb.linearVelocity.normalized;
-            if (Mathf.Abs(v.x) < 0.01f)
-            {
-                v.x = 0.1f * Mathf.Sign(Random.Range(-1f, 1f));
-            }
-            if (Mathf.Abs(v.y) < 0.01f)
-            {
-                v.y = 0.1f * Mathf.Sign(Random.Range(-1f, 1f));
-            }
-            rb.linearVelocity = v.normalized * speed;
+            FixBallDirection();
         }
+    }
+
+    private void FixBallDirection()
+    {
+        rb.linearVelocity = rb.linearVelocity.normalized * speed;
+
+        Vector2 v = rb.linearVelocity.normalized;
+        if (Mathf.Abs(v.x) < 0.01f)
+        {
+            v.x = 0.1f * Mathf.Sign(Random.Range(-1f, 1f));
+        }
+        if (Mathf.Abs(v.y) < 0.01f)
+        {
+            v.y = 0.1f * Mathf.Sign(Random.Range(-1f, 1f));
+        }
+        rb.linearVelocity = v.normalized * speed;
     }
 
     private void HandlePaddleCollision(Collision2D collision)
